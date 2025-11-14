73f326700f460e060c9ae2fe52187cda06f12476
funnieguy <sebastian.olah220@gmail.com>
2025-11-14 01:32:19 -0800

    Added some error handling with souper() function. Makes it more reliable.

--- web_scraper.py
+++ web_scraper.py
@@ -1,11 +1,18 @@
 from urllib.request import urlopen
+import urllib.error
 import re as regex
 from bs4 import BeautifulSoup as Soup
 
+
+
 #function for taking in website and making a beautiful soup object. The return can be used to be passed into other bs methods
 def souper(grabbed_url):
-    open_page = urlopen(grabbed_url)
-    raw_html = open_page.read().decode()
+    try:
+        open_page = urlopen(grabbed_url)
+        raw_html = open_page.read().decode()
+    except (urllib.error.HTTPError, urllib.error.URLError, ValueError):
+        print("Error: unable to fetch page.")
+        return None
 
     return Soup(raw_html, 'html.parser')
 
@@ -13,7 +20,8 @@ def link_getter(soup_obj):
     list_of_links = []
 
     for tag in soup_obj.find_all('a'):
-        list_of_links.append(tag['href'])
+        if 'href' in tag.attrs:
+            list_of_links.append(tag['href'])
 
     return list_of_links
 
@@ -21,26 +29,7 @@ def img_getter(soup_obj):
     list_of_imgs = []
 
     for tag in soup_obj.find_all('img'):
-        list_of_imgs.append(tag["src"])
+        if 'src' in tag.attrs:
+            list_of_imgs.append(tag["src"])
 
     return list_of_imgs
-
-
-#file1 = open(r"C:\Users\OLAHg\Desktop\web_scraper\stored_image_sources.txt", "w+")
-
-
-
-
-#print(img_getter(souper("https://www.pinterest.com/ideas/wallpapers/951361127909/")))
-
-
-#image_list = souper.find_all("img")
-#print(image_source)
-
-#pattern_1 = "<title.*?>.*?</title.*?"
-#search_operand = regex.search(pattern_1, html, regex.IGNORECASE)
-#title = search_operand.group()
-
-
-
-
